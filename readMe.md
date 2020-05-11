6fdc5ca556dba4818b12089c6675c258d39b0b5b
Ma7eer <maher.alkendi2@gmail.com>
2020-05-12 01:22:21 +0400

    added end to client

---
+++ readMe.md
@@ -0,0 +1,1 @@
+https://chartio.com/resources/tutorials/how-to-define-an-auto-increment-primary-key-in-postgresql/
--- routes/index.js
+++ routes/index.js
@@ -7,8 +7,10 @@ const db = require("../database");
 router.get("/", function (req, res, next) {
   db.query("SELECT * FROM stations", (error, results1) => {
     if (error) throw error;
+    // console.log(results1);
     db.query("SELECT * FROM entries", (error, results2, fields) => {
       if (error) throw error;
+      db.end();
       return res.render("index", {
         entries: results2.rows,
         station: results1.rows[0],
